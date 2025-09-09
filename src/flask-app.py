cd7999ce2aec1e314496ed424c1add9320226440
kee <notthad@gmail.com>
2025-09-09 16:12:01 +0800

    Updated flask app code

--- src/flask-app.py
+++ src/flask-app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def home():
-    return "hi"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
