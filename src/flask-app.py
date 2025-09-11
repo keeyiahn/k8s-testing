2c108ddc0e066d07473197fa403b1879ce140997
kee <notthad@gmail.com>
2025-09-11 16:58:13 +0800

    Added POST request functionality to connect to SQL server

--- src/flask-app.py
+++ src/flask-app.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template, jsonify, request
+import psycopg2
+def connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="flaskdb",
+        user="flaskuser",
+        password="flaskpassword",
+        port=5432
+    )
+    cursor = conn.cursor()
+    return conn, cursor
+
 app = Flask(__name__, template_folder="../templates")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+
 @app.route("/post", methods=["POST"])
-def handle_post():
+def add_to_db():
     data = request.get_json(silent=True)
     if not data:
         return jsonify({"error": "Invalid or missing JSON"}), 400
 
-    name = data.get("name", "Guest")
+    name = data.get("name")
+    age = data.get("age")
+
+    conn, cursor = connection()
+    cursor.execute("INSERT INTO users (name, age) VALUES (%s, %s)", (name, age))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
     return jsonify({
         "message": f"Hello, {name}! JSON received successfully.",
